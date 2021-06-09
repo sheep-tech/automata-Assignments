94c5c0ac10032af6cb5306a07f8de2b5a5263251
Arvin Firouzi <427014@student.fontys.nl>
2021-06-09 13:33:48 +0200

    some changes

--- init.bat
+++ init.bat
@@ -2,7 +2,7 @@ PATH=%PATH%;"C:\Program Files\Java\jdk16.0.1\bin"
 SET CLASSPATH=%CLASSPATH%;obj;antlr-4.9.2-complete.jar
 doskey a4=java org.antlr.v4.Tool MyGrammar.g4 -o gen
 doskey jc=javac MyGrammar*.java Main.java -d obj
-doskey grun=java org.antlr.v4.gui.TestRig MyGrammar myStart -gui input.txt
+doskey grun=java org.antlr.v4.gui.TestRig MyGrammar start -gui input.txt
 doskey run=java Main $L input.txt
 doskey clean=del /Q gen\* obj\*
 
