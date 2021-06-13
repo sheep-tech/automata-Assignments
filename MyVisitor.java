e09a246c75188a273f88fec2064e9465faf5f2db
Matteo Zhang <zhang.matteo00@gmail.com>
2021-06-13 22:26:16 +0200

    prettier parse

--- MyVisitor.java
+++ MyVisitor.java
@@ -19,24 +19,34 @@ public class MyVisitor extends MyGrammarBaseVisitor<Value> {
     @Override
     public Value visitPrint(MyGrammarParser.PrintContext ctx) {
         int rowIndex = 1;
+
+        System.out.println("+---+---+---+");
         for (Map.Entry<String, Value> a : memory.entrySet()) {
             String id = a.getKey();
 
-            if (Character.getNumericValue(id.charAt(1)) != rowIndex) {
+            if ((Integer.parseInt(id.substring(2))) == 1) {
+                System.out.print("|");
+            }
+
+            System.out.print(a.getValue());
+
+            if ((Integer.parseInt(id.substring(2)) % 3) == 0 ) {
+                System.out.print("|");
+            }
+
+            if (Character.getNumericValue(id.charAt(2)) == 9) {
                 rowIndex++;
                 System.out.println();
-                System.out.println("-------------------------");
             }
-
-            System.out.print(a.getValue() + " ");
-
-            if ((Integer.parseInt(id.substring(2)) % 3) == 0) {
-                System.out.print(" | ");
-            }
-
+            printNewLine(id);
         }
 
         return Value.VOID;
     }
 
+    private void printNewLine(String id) {
+        if ((Character.getNumericValue(id.charAt(1)) % 3) == 0 && Character.getNumericValue(id.charAt(2)) == 9) {
+            System.out.println("+---+---+---+");
+        }
+    }
 }
